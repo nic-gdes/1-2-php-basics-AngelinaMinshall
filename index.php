5fa29394e832244ecb9f0d8d0862c61aba65732f
Angelina Minshall <atminshall@nic.edu>
2024-01-17 16:14:43 -0800

    creating vars, echos, conditionals, loops

---
+++ index.php
@@ -0,0 +1,68 @@
+<?php
+// Variable
+$string = "I had a dream";
+$integer = 7;
+$float = 3.14
+$boolean = True;
+
+echo "I " . "love " . "PHP";
+
+$myarray = [1,2,3,4];
+
+ class Car = {
+    public $make;
+    function set_make($make) {
+        $this->make = $make;
+    }
+    public $model;
+    function set_model($model) {
+        $this ->model =$model;
+    }
+    public $year;
+    function set_year($year) {
+        $this ->year =$year;
+ } 
+}
+
+
+ $lamabo = new Car {
+    $lambo -> set_make("Lambo");
+ }
+
+ $newYear = new Year {
+    $newYear -> set_year(2001);
+ }
+
+ $revuelto = new Model {
+    $revuelto -> set_model("Revuelto");
+ }
+
+ echo("I have a " . $newYear . $lambo . $revuelto ".")
+
+
+ // Conditionals
+
+ $interger = 15;
+
+ if ($integer >= 10) {
+    echo "The statement is fr";
+ } else {
+    echo "The statement is cap":
+ }
+
+ //Standard
+
+ for ($i = 0; $i <count($myArray); $i++){
+    echo $myArray[$i] . "<br>";
+ }
+
+ //For Each
+foraach ($myArray as $item) {
+    echo $item . "<br>";
+} 
+
+?>
+
+
+
+ 
